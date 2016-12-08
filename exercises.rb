3845b09316b99f7515de6336bed0cdeb712ad625
Andrew James <ajames20@gmail.com>
2016-12-08 12:33:51 +0200

    formatting edit

--- exercises.rb
+++ exercises.rb
@@ -66,8 +66,8 @@ puts 12.2 * 12.2
 puts 129.8 * 129.8
 puts "\n"
 
-=begin exercise 7: What does the following error message tell you?
-
+=begin
+exercise 7: What does the following error message tell you?
 SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
   from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
 =end
